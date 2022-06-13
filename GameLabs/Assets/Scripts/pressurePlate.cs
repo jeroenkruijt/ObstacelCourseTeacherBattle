287e691927084fa0426f45c4d985b7f05385c27d
Lenna <maehorn0912@gmail.com>
2022-06-13 22:05:14 +0200

    animator fixed my gosh
    stupid pressure plate istg

--- GameLabs/Assets/Scripts/pressurePlate.cs
+++ GameLabs/Assets/Scripts/pressurePlate.cs
@@ -10,15 +10,20 @@ namespace Scripts
         [SerializeField]
         private GameObject connectedBox;
         public Animator _anim;
+
+        public void Start()
+        {
+            _anim = GetComponent<Animator>();
+        }
         public void OnTriggerEnter2D(Collider2D other)
         {
             //if a player steps on this call the attached box' move function
             if (other.CompareTag("Player"))
             {
-                FindObjectOfType<AudioManager>().Play("switch");
+                //FindObjectOfType<AudioManager>().Play("switch");
                 _anim.SetBool("stepped", true);
                 moveable connected = connectedBox.GetComponent<moveable>();
-                connected.SendMessage("move");
+                //connected.SendMessage("move");
                 Debug.Log("*gets pounced on*");
             }
         }
